fc7949924e19b4dd1c13f4f370f421673fb342c9
Jie Li <lijiemessage@gmail.com>
2020-12-12 00:03:10 +0800

    update our menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom'
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path = '/' component = {HomePage} />
     </div>
   );
 }
