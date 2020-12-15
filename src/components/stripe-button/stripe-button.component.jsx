eace410c473ba374d8c3ee29d1ec30fa8df5feba
Jie Li <lijiemessage@gmail.com>
2020-12-15 15:40:11 +0800

    integrating stripe payment and updating title and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HyWbRBnzpvPRDkUxMCh427YnEWGzspI19YZfNOPgoHN3SIAx8ZVcsLj1bMaZe2KC7dqHnZLxWA8XkVKjnMFMpGP00gqq0sHpi';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      alipay
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
